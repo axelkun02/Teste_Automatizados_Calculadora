70968f0e56e57ea24f38054bba1a486d6fd25818
Axel Alves <113148503+axelkun02@users.noreply.github.com>
2023-11-07 17:17:06 -0300

    Projeto da calculadora atualizado
    Projeto implementado com sua documentação.

---
+++ Calculadora_Documentacao/src/calculadora/divisao.java
@@ -0,0 +1,23 @@
+package calculadora;
+
+/**
+*@author Axel Alves
+*@version 1.02
+*@since 1.00
+*
+* Classe que executa a operacao de divisao.
+* 
+* @param a1 variavel que representa o primeiro numero a ser usado na divisao.
+* @param a2 variavel que representa o segundo numero a ser usado na divisao.
+* @return traz o retorno da variavel div, que representa a divisao das 
+* variaveis a1 e a2.
+*/
+
+public class divisao {
+	 public int dividir (int a1, int a2){
+		    int div = a1 / a2;
+		    return div; // retorno da divisao
+		    
+		    }
+
+}
